094dc36f99c50ca603567367bb3f92e04d55028a
payyer <quocanhle112@gmail.com>
2024-07-09 07:57:43 +0700

    Init socketIO

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const connectToDB = require('./src/db')
 const cookieParser = require('cookie-parser')
 const router = require('./src/routes');
 const app = express()
+const http = require('http');
+const { Server } = require('socket.io');
 
 // setup middleware
 app.use(cors({ origin: "http://localhost:5173", credentials: true }))
@@ -17,6 +19,22 @@ connectToDB()
 // routes
 app.use('/api/v2', router)
 
-app.listen(8000, () => {
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+    },
+});
+let onlineUsers = []
+io.on('connection', (socket) => {
+    console.log(`User connected ${socket.id}`);
+    socket.on('addNewUser', (userId) => {
+        !onlineUsers.some((user) => user.userId === userId) &&
+            onlineUsers.push({ userId, socket: socket.id })
+    });
+});
+
+server.listen(8000, () => {
     console.log("Listen on port 8000")
 })
